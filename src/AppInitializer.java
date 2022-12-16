4f2a60f896f96c705777834ff79292ded26cda68
oshadaera68 <mindartlk010@gmail.com>
2022-12-16 15:00:06 +0530

    updated content

--- src/AppInitializer.java
+++ src/AppInitializer.java
@@ -14,7 +14,7 @@ public class AppInitializer extends Application {
     @Override
     public void start(Stage primaryStage) throws IOException {
         primaryStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("view/MainView.fxml"))));
-        primaryStage.setTitle("Wizard v0.1");
+        primaryStage.setTitle("IntelliJ IDEA Setup");
         primaryStage.show();
     }
 }
